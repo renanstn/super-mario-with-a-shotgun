f0ffde55d27f7294de556ba3e9d15765a3dbcfbd
Renan Santana <renan_ota@hotmail.com>
2021-07-14 22:58:36 -0300

    Finaliza animação do personagem

--- Super Mario With a Shotgun/Scripts/Mario.gd
+++ Super Mario With a Shotgun/Scripts/Mario.gd
@@ -5,18 +5,21 @@ const UP: Vector2 = Vector2(0, -1)
 const GRAVITY: int = 15
 const JUMP: int = 350
 
-export var speed: int = 200
-var motion: Vector2 = Vector2()
-
 onready var sprite : Sprite = $Sprite
-#onready var animator : AnimationPlayer = $AnimationPlayer
+onready var animator : AnimationPlayer = $AnimationPlayer
+
+export var speed: int = 150
+var motion: Vector2 = Vector2()
+var looking_to_right: bool = false
 
 signal player_flipped(direction)
+
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 	motion = move_and_slide(fromInputsToMotion(), UP)
-#	animate()
+	animate()
+
 
 func fromInputsToMotion() -> Vector2:
 	if Input.is_action_pressed("move_right"):
@@ -28,19 +31,20 @@ func fromInputsToMotion() -> Vector2:
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		motion.y = -JUMP
 	return motion
-#
-#func animate() -> void:
-#	if motion.x != 0:
-#		animator.play("Walking")
-#	else:
-#		animator.play("Idle")		
-#	if !is_on_floor():
-#		animator.play("Jumping")
-#	if motion.x > 0 and sprite.flip_h:
-#		sprite.flip_h = false
-#		# Emitir o signal para o script da câmera2D
-#		emit_signal("player_flipped", "right")
-#	if motion.x < 0 and !sprite.flip_h:
-#		sprite.flip_h = true
-#		# Emitir o signal para o script da câmera2D
-#		emit_signal("player_flipped", "left")
+
+
+func animate() -> void:
+	if motion.x != 0:
+		animator.play("Walking")
+	else:
+		animator.play("Idle")
+	if !is_on_floor():
+		animator.play("Jumping")
+	if motion.x > 0 and not looking_to_right:
+		looking_to_right = true
+		scale.x = -1
+		emit_signal("player_flipped", "right")
+	if motion.x < 0 and looking_to_right:
+		looking_to_right = false
+		scale.x = -1
+		emit_signal("player_flipped", "left")

ce85ec5df65f155f3c11a5bed1f08d8aa03cdda3
Renan <renan_ota@hotmail.com>
2025-09-08 19:14:40 -0300

    wip

--- new-version/scripts/koopa.gd
+++ new-version/scripts/koopa.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+const SPEED = 50.0
+var direction := -1
+var sprite_flipped := true
+
+func _physics_process(delta: float) -> void:
+	velocity.x = direction * SPEED
+	move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
 
 func get_hit():
 	print("ai meu cusin!!!!")

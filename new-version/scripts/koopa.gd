8dc76f852439166c9a48fd1248055dfd39845e1a
Renan <renan_ota@hotmail.com>
2025-10-06 23:10:58 -0300

    Add die animation to koopa

--- new-version/scripts/koopa.gd
+++ new-version/scripts/koopa.gd
@@ -1,14 +1,36 @@
 extends CharacterBody2D
 
 const SPEED = 50.0
+const DEATH_KNOCKBACK = 150.0
+const DEATH_JUMP = -200.0
+const DEATH_FRICTION = 500.0
+const DEATH_ROTATION_SPEED = 180.0
 var direction := -1
 var sprite_flipped := true
+var alive := true
+var target_rotation := 0.0
 
 func _physics_process(delta: float) -> void:
-	velocity.x = direction * SPEED
-	move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+	if alive:
+		velocity.x = direction * SPEED
+		move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
+		$AnimationPlayer.play("walking")
+	else:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			if rotation_degrees < target_rotation:
+				rotation_degrees = min(rotation_degrees + DEATH_ROTATION_SPEED * delta, target_rotation)
+		else:
+			velocity.x = move_toward(velocity.x, 0, DEATH_FRICTION * delta)
+		move_and_slide()
 
 
 func get_hit():
-	print("ai meu cusin!!!!")
+	if not alive:
+		return
+	alive = false
+	$AnimationPlayer.play("dying")
+	target_rotation = 90
+	velocity.x = -direction * DEATH_KNOCKBACK
+	velocity.y = DEATH_JUMP
